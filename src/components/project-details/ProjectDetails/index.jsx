b77a05ee2aca0640559d256b2a366908c9ee6591
Bence Hornák <bence.hornak@gmail.com>
2020-08-05 17:10:17 +0200

    Add header to ProjectDetails

--- src/components/project-details/ProjectDetails/index.jsx
+++ src/components/project-details/ProjectDetails/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Container } from 'components/common';
-import { Wrapper, Header, Screenshot } from './styles';
+import { Header } from 'components/theme';
+import { Wrapper, ProjectHeader, Screenshot } from './styles';
 
 export const ProjectDetails = ({ post, screenshot }) => (
-  <Wrapper as={Container}>
-    <Header>
-      <h1>{post.frontmatter.title}</h1>
-      <Screenshot src={screenshot.publicURL} />
-    </Header>
-    {/* eslint-disable-next-line react/no-danger */}
-    <div dangerouslySetInnerHTML={{ __html: post.html }} className="blog-post-content" />
-  </Wrapper>
+  <>
+    <Header />
+    <Wrapper as={Container}>
+      <ProjectHeader>
+        <h1>{post.frontmatter.title}</h1>
+        <Screenshot src={screenshot.publicURL} />
+      </ProjectHeader>
+      {/* eslint-disable-next-line react/no-danger */}
+      <div dangerouslySetInnerHTML={{ __html: post.html }} className="blog-post-content" />
+    </Wrapper>
+  </>
 );
--- src/components/project-details/ProjectDetails/styles.js
+++ src/components/project-details/ProjectDetails/styles.js
@@ -4,7 +4,7 @@ export const Wrapper = styled.div`
   padding: 4rem 0;
 `;
 
-export const Header = styled.div`
+export const ProjectHeader = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
